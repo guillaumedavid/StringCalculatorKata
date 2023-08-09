b405b63761588329d9c62301df2e203b6a4212b5
GD0B9A5L <guillaume-externe.david@enedis.fr>
2023-08-09 09:21:42 +0200

    part1-solution

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,16 @@
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public class StringCalculator {
+
+    public static String sum(String input) {
+        if (input.isEmpty()) {
+            return "0";
+        }
+        return Arrays.stream(input.split(","))
+                .map(BigDecimal::new)
+                .reduce(BigDecimal::add)
+                .get()
+                .toPlainString();
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,46 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StringCalculatorTest {
+
+
+    @Test
+    void shouldGetReturnSingleValue(){
+        //GIVEN
+        String input = "1";
+        //WHEN
+        String result = StringCalculator.sum(input);
+        //THEN
+        Assertions.assertThat(result).isEqualTo("1");
+    }
+
+    @Test
+    void shouldGetZeroForEmptyInput(){
+        //GIVEN
+        String input = "";
+        //WHEN
+        String result = StringCalculator.sum(input);
+        //THEN
+        Assertions.assertThat(result).isEqualTo("0");
+    }
+
+    @Test
+    void shouldSumIntegers(){
+        //GIVEN
+        String input = "1,2";
+        //WHEN
+        String result = StringCalculator.sum(input);
+        //THEN
+        Assertions.assertThat(result).isEqualTo("3");
+    }
+
+    @Test
+    void shouldSumDecimals(){
+        //GIVEN
+        String input = "1.1,2.2";
+        //WHEN
+        String result = StringCalculator.sum(input);
+        //THEN
+        Assertions.assertThat(result).isEqualTo("3.3");
+    }
 }
